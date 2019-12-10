5d2c35c8092374b39c9e079be49e26505ec1e422
Dev Ops <testdevops2017@yandex.com>
2019-12-10 17:13:55 +0300

    post için test eklendi

--- MavenBuildGroup/src/Driver.java
+++ MavenBuildGroup/src/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
 	private static String pJson;
 	
 	public static void main(String[] args) throws IOException {
-		//testPostJson();
+		testPostJson();
 		testMainBuild();
 		
 	}
