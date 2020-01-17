432920cfe8edf0ac9969c0385c59ee09787086b0
Mustafa ALP <alp.mustf@gmail.com>
2020-01-17 16:57:14 +0300

    Update Main.java

--- MavenBuildGroup/src/Main.java
+++ MavenBuildGroup/src/Main.java
@@ -1,10 +1,20 @@
+package BuilSocketServer;
+
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.maven.shared.invoker.MavenInvocationException;
 import org.eclipse.jgit.api.errors.ConcurrentRefUpdateException;
@@ -19,45 +29,65 @@ import org.json.JSONObject;
 
 public class Main {
 
-    private static String login;
-    private static String password;
-    private static String  url;
-    private static String  projectName;
-    private static String JsonStr;
-    private static String object_type;
-    private static String card_id;
-    private static String method;
-    private static String destination;
-    private static String fileName = new SimpleDateFormat("yyyyMMddHHmm'.txt'").format(new Date());
+    private  String login;
+    private  String password;
+    private  String  url;
+    private  String  projectName;
+    private  String JsonStr;
+    private  String object_type;
+    private  String card_id;
+    private  String method;
+    private  String destination;
+    private  String fileName ;
     
 
-    public static void build(String pJson) {
+    public  void build(String pJson) throws IOException {
 
         try {
 
             JsonStr = pJson;
             parse_Json();
-
-            PrintStream out = new PrintStream(new FileOutputStream(fileName));
-            System.setOut(out);
             
-         
+            fileName = "report-" + new SimpleDateFormat("yyyyMMddHHmmSSS'.txt'").format(new Date());
+            System.err.println("\n\n"+ fileName + "\n\n");
+            
+            
+            PrintStream sys = System.out; 
+            FileOutputStream reportFile = new FileOutputStream("report.txt", false);
+            reportFile.flush();
+            
+            PrintStream out = new PrintStream(reportFile, true, StandardCharsets.UTF_8.toString());
+            System.setOut(out);            
+//            File delete = new File("report.txt");        
+//            delete.delete();
+            
+
+            
             Maven_Builder project = new Maven_Builder(url , projectName , login , password);
 
+            System.err.print("BUILD STARTED0\n");
             project.build(); //build project
+            System.err.print("BUILD STARTED1\n");
             
+            System.setOut(sys);
+//            reportFile.close();
+    		System.out.println("\nhelloo world");
+
 
             Boolean signal  = null; //to send request
-            BufferedReader br = new BufferedReader(new FileReader(fileName)); //read report from the file
+            BufferedReader br = new BufferedReader(new FileReader("report.txt")); //read report from the file
 
             String sCurrentLine;
             String report = new String();
 
             while ((sCurrentLine = br.readLine()) != null) {
-                report += sCurrentLine;
+//            	System.err.print(sCurrentLine+"\n");
+//            	if(sCurrentLine.charAt(0) != '\u0000')
+            		report += sCurrentLine;
             }
             
             if(report.contains("BUILD SUCCESS")){
+            	System.err.print("BUILD SUCCESS\n");
                 signal = true; // request signal to send
             }
             else {
@@ -66,72 +96,120 @@ public class Main {
             br.close();
 
             project.push(signal);
+            String commit;
 
             if(signal) {
             	destination = "7";
+            	commit = project.getCommitID();
+    			// JSon objesi hazirlama ...
+                JSONObject jsonTest = new JSONObject();
+                jsonTest.put("destination", destination);
+                jsonTest.put("object_type", "response");
+                jsonTest.put("origin", "1");
+    			jsonTest.put("operation", "1");
+    			jsonTest.put("status", signal.toString());
+    			jsonTest.put("description", report);
+    			jsonTest.put("commit_ID", commit);
+    			jsonTest.put("github_login", login.toString());
+    			jsonTest.put("github_password", password.toString());
+    			jsonTest.put("repository_url", url.toString());
+    			jsonTest.put("project_name", projectName.toString());
+    			jsonTest.put("method", "check-build-status");
+    			
+    			String postUrl = "http://localhost:8081";
+    			
+    			System.err.print("Test response " + HttpRequest.PostJson(postUrl, jsonTest.toString()));
+    			
+    			destination = "2";
+    			// JSon objesi hazirlama ...
+                JSONObject jsonPLan = new JSONObject();
+                jsonPLan.put("destination", destination);
+                jsonPLan.put("object_type", "response");
+                jsonPLan.put("origin", "1");
+                jsonPLan.put("operation", "1");
+                jsonPLan.put("status", signal.toString());
+                jsonPLan.put("description", report);
+                jsonPLan.put("commit_ID", commit);
+                jsonPLan.put("github_login", login.toString());
+                jsonPLan.put("github_password", password.toString());
+                jsonPLan.put("repository_url", url.toString());
+                jsonPLan.put("project_name", projectName.toString());
+                jsonPLan.put("method", "check-build-status");
+    			
+    			
+    			System.err.print("PLan response " + HttpRequest.PostJson(postUrl, jsonPLan.toString()));
             }
             else {
             	destination = "2";
+                commit = "0";
+    			// JSon objesi hazirlama ...
+                JSONObject json = new JSONObject();
+    			json.put("destination", destination);
+    			json.put("object_type", "response");
+    			json.put("origin", "1");
+    			json.put("operation", "1");
+    			json.put("status", signal.toString());
+    			json.put("description", report);
+    			json.put("commit_ID", commit);
+    			json.put("github_login", login.toString());
+    			json.put("github_password", password.toString());
+    			json.put("repository_url", url.toString());
+    			json.put("project_name", projectName.toString());
+    			json.put("method", "check-build-status");
+    			
+    			String postUrl = "http://localhost:8081";
+    			
+    			System.err.print("response Fail" + HttpRequest.PostJson(postUrl, json.toString()));
             }
 
-            String json_res = new String();
-			json_res += "{\n"+
-					"\"destination\" : " + destination + "\",\n"+
-                    "\"object_type\" : \"response\" ,\n"+
-                    "\"status\" :  \"" + signal.toString()+ "\",\n"+
-                    "\"description\" : \"" + report + "\",\n"+
-                    "\"commit_ID\" : \"" +project.getCommitID() + "\",\n"+
-                    "\"github_login\" : \" " + login.toString() + "\",\n"+
-                    "\"github_password\" : \"" + password.toString() + "\",\n"+
-                    "\"repository_url\" : \"" + url.toString() + "\",\n"+
-                    "\"project_name\" : \"" + projectName.toString() + "\" ,\n"+
-                    "\"method\" : \"check-build-status\"\n"+
-                    "} ";
-			
-			String postUrl = "http://localhost:8081";
-			String url  = "http://dummy.restapiexample.com/api/v1/create";
+            
+//			System.err.print("\n\n\nREPORT: " + report + "\n");
 
-			System.err.print("responce " + HttpRequest.PostJson(postUrl, json_res));
 			
+			//out.flush(); Truncate etmenin yolunu bul
 
         } catch (NoFilepatternException e) {
             System.err.println(e.toString());
-            System.exit(1);
+            
         } catch (WrongRepositoryStateException e) {
             System.err.println(e.toString());
-            System.exit(1);
-        } catch (JGitInternalException e) {
+           
+        } catch (JGitInternalException e){
             System.err.println(e.toString());
-            System.exit(1);
+           
+            Runtime.getRuntime().exec("rm -r /tmp/" + projectName.toString()); //remove directory
+            
         } catch (ConcurrentRefUpdateException e) {
             System.err.println(e.toString());
-            System.exit(1);
+            
         } catch (NoMessageException e) {
             System.err.println(e.toString());
-            System.exit(1);
+            
         } catch (NoHeadException e) {
             System.err.println(e.toString());
-            System.exit(1);
+            
         } catch (InvalidRemoteException e) {
             System.err.println(e.toString());
-            System.exit(1);
+            
         } catch (IOException e) {
             System.err.println(e.toString());
-            System.exit(1);        
+                    
         } catch (JSONException e) {
             System.err.println(e.toString());
-            System.exit(1);        
+                    
         } catch (MavenInvocationException e) {
-        	System.err.println("");
+        	
             System.err.println(e.toString());
-            System.exit(1);     
+                 
         } catch (Exception e) {
             System.err.println(e.toString());
-            System.exit(1);      
-        }finally {
-            File delete = new File(fileName);
-            delete.delete();
-        }
+                  
+        }/*finally {
+            
+        }*/
+        /*File delete = new File("report.txt");        
+        delete.delete();*/
+        
     }
     
     /**
@@ -140,7 +218,7 @@ public class Main {
      *  
      * @throws JSONException 
      */
-    private static void parse_Json() throws JSONException   {
+    private  void parse_Json() throws JSONException   {
     	
 
         JSONObject obj = new JSONObject(JsonStr);
@@ -153,7 +231,7 @@ public class Main {
         
         //url = "https://" + url.substring(4) + ".git";
         
-        card_id = obj.getString("card_id");
+        //card_id = obj.getString("card_id");
 
 
     }
